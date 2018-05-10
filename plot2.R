01c0cf952e10d967020d025a65fa7fb06032566c
Harney <aharney@arraybiopharma.com>
2018-05-10 12:05:28 -0600

    modified to use dplyr

--- plot2.R
+++ plot2.R
@@ -1,5 +1,5 @@
 ##Plotting the household energy usage over a 2-day period in Feb 2007
-##Plot1 reconstructs line plot of Global Active Power over each day
+##Plot1 reconstructs x-y line plot of Global Active Power over each day
 
 ##download and unzip the dataset
 if(!file.exists("./data")){dir.create("./data")}
@@ -12,10 +12,15 @@ unzip(zipfile = "./data/dataset.zip", exdir = "./data")
 ##read in data table
 power <- read.table("./data/household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
 
-##subset on selected dates
-power2 <- power[(power$Date == "1/2/2007" | power$Date == "2/2/2007"),]
+##subset on selected dates and change format of date and time
+library(dplyr)
 
-##plot histogram of Global Active Power and export png file
-png(filename = "plot1.png", width = 480, height = 480, units = "px")
-hist(power2$Global_active_power, main = "Global Active Power", col = "red", border = "black", xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
+power2 <- filter(power, Date == "2/2/2007" | Date == "1/2/2007") %>%  
+  mutate(GAP = as.numeric(Global_active_power)) %>%   
+  mutate(date_time = as.POSIXct(paste(Date,Time), format="%d/%m/%Y %H:%M:%S"))  
+
+#plot x-y line graph 
+png(filename="plot2.png", width = 480, height = 480)
+
+plot(power2$date_time, power2$GAP, type ="l", ylab = "Global Active Power (kilowatts)", xlab = "")
 dev.off()
